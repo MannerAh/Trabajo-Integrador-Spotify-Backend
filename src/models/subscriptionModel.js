562e32ead9124c07bdcb60d705a3f019e1369077
MannerAh <leoncitobaro@hotmail.es>
2025-10-22 01:40:07 -0300

    fix renewal and start date

--- src/models/subscriptionModel.js
+++ src/models/subscriptionModel.js
@@ -1,6 +1,6 @@
 // models/subscription.js
 const { DataTypes } = require('sequelize');
-const {sequelize} = require('../config/database');
+const { sequelize } = require('../config/database');
 
 const Subscription = sequelize.define('Subscription', {
     id: {
@@ -12,13 +12,26 @@ const Subscription = sequelize.define('Subscription', {
         type: DataTypes.STRING(9),
         allowNull: false,
     },
-    // user_id será manejado por la asociación
+    // Clave Foránea (Foreign Key)
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+
+    start_date: {
+        type: DataTypes.DATE, 
+        allowNull: true,
+    },
+    renewal_date: {
+        type: DataTypes.DATE, 
+        allowNull: true,
+    }
 }, {
     tableName: 'subscription',
     timestamps: true,
-    createdAt: 'create_time',
-    updatedAt: 'update_time',
-    underscored: true,
+    createdAt: 'create_time', // Mapea create_time a createdAt
+    updatedAt: 'update_time', // Mapea update_time a updatedAt
+    underscored: true, 
 });
 
 module.exports = Subscription;
